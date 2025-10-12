24473b237da0d9f1a4bc158d9acc9c768143acf1
jan <git@lolmerkat.cc>
2025-10-13 00:07:22 +0200

    refactor(nvim): move markview toggle keybind

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -2,4 +2,11 @@ return {
     "oxy2dev/markview.nvim",
     lazy = false,
     priority = 49,
+    keys = {
+        {
+            "<leader>mm",
+            "<cmd>MarkviewToggle<CR>",
+            desc = "Toggle Markview Split"
+        }
+    }
 }
