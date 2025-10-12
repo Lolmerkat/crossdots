cd74aa35e295cd12cc9a320681ec0e8e3ed17f12
Jan Meyer <git@lolmerkat.cc>
2025-10-13 00:25:06 +0200

    feat(nvim): add ltex and jdtls lsp config

---
+++ nvim/lua/lsp/jdtls.lua
@@ -0,0 +1,21 @@
+return {
+    filetypes = { "java", "class" },
+    root_markers = {
+        "pom.xml",
+        "build.gradle",
+        "build.gradle.kts",
+        ".git"
+    },
+    -- settings = {
+    --     java = {
+    --         format = { enabled = true },
+    --         configuration = {
+    --             runtimes = {},
+    --             excluded = {
+    --                 "build",
+    --                 "bin"
+    --             }
+    --         }
+    --     },
+    -- },
+}
