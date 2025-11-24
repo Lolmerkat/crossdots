a45ba18fde5b6fcd4c79c6bc6c6e1b80d6def9af
Jan Meyer <git@lolmerkat.cc>
2025-11-24 21:20:52 +0100

    feat(nvim): update ensure_installed lsps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,12 @@ return {
         "mason-org/mason-lspconfig.nvim",
         opts = {
             automatic_enable = true,
-            ensure_installed = { "lua_ls" }
+            ensure_installed = {
+                "lua_ls",
+                "emmet_language_server",
+                "prettierd",
+                "clangd"
+            }
         },
         dependencies = {
             "neovim/nvim-lspconfig",
