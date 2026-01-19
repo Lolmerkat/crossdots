6edf89384b2b5a275981b4a4d31ae3050686ecbd
Jan Meyer <git@lolmerkat.cc>
2026-01-19 14:40:24 +0100

    feat(wezterm): use matugen style

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,22 +3,26 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.keys = {
-	{ key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-	{ key = 'C', mods = 'CTRL|SHIFT', action = act.CopyTo 'ClipboardAndPrimarySelection' },
-    { key = 'W', mods = 'CTRL|SHIFT', action = act.CloseCurrentTab {confirm = true} },
+    { key = 'V', mods = 'CTRL',       action = act.PasteFrom 'Clipboard' },
+    { key = 'C', mods = 'CTRL|SHIFT', action = act.CopyTo 'ClipboardAndPrimarySelection' },
+    { key = 'W', mods = 'CTRL|SHIFT', action = act.CloseCurrentTab { confirm = true } },
     { key = 'N', mods = 'CTRL|SHIFT', action = act.ToggleFullScreen },
     { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
     { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(1) },
-    { key = 'h', mods = 'CTRL|ALT', action = act.MoveTabRelative(-1) },
-    { key = 'l', mods = 'CTRL|ALT', action = act.MoveTabRelative(1) },
-    { key = 'R', mods = 'CTRL|SHIFT', action = act.PromptInputLine {
-        description = "Enter a new name for the tab",
-        action = wezterm.action_callback(function (window, _, line)
-            if line then
-                window:active_tab():set_title(line)
-            end
-        end)
-    }},
+    { key = 'h', mods = 'CTRL|ALT',   action = act.MoveTabRelative(-1) },
+    { key = 'l', mods = 'CTRL|ALT',   action = act.MoveTabRelative(1) },
+    {
+        key = 'R',
+        mods = 'CTRL|SHIFT',
+        action = act.PromptInputLine {
+            description = "Enter a new name for the tab",
+            action = wezterm.action_callback(function(window, _, line)
+                if line then
+                    window:active_tab():set_title(line)
+                end
+            end)
+        }
+    },
 }
 
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
@@ -32,11 +36,11 @@ config.enable_wayland = false
 config.window_background_opacity = 0.44
 config.win32_system_backdrop = 'Acrylic'
 config.kde_window_background_blur = true
-config.colors = require("cyberdream")
 config.font = wezterm.font_with_fallback {
     { family = 'JetBrains Mono', weight = 'Medium' },
     'Noto Color Emoji'
 }
+config.color_scheme = "matugen_theme"
 
 -- and finally, return the configuration to wezterm
 return config
